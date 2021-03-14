dae6702601a51fdebb34315e84b7f19b0f19313d
Flame丶 <sweet.sakura@qq.com>
2021-03-15 01:15:42 +0800

    表结构修改，新增教练id 嵌套结构体

--- server/model/student.go
+++ server/model/student.go
@@ -1,20 +1,9 @@
 // 自动生成模板Student
 package model
 
-import (
-	"Driving-school/global"
-)
-
 // 如果含有time.Time 请自行import time包
 type Student struct {
-	global.GVA_MODEL
-	AuthorityId   string `json:"authorityId" form:"authorityId" gorm:"column:authority_id;comment:用户角色ID;type:varchar(90);size:90;default:200"`
-	HasServerInfo *bool  `json:"hasServerInfo" form:"hasServerInfo" gorm:"column:has_server_info;comment:;type:tinyint;"`
-	HeaderImg     string `json:"headerImg" form:"headerImg" gorm:"column:header_img;comment:用户头像;type:varchar(191);size:191;"`
-	NickName      string `json:"nickName" form:"nickName" gorm:"column:nick_name;comment:用户昵称;type:varchar(191);size:191;"`
-	Password      string `json:"password" form:"password" gorm:"column:password;comment:用户登录密码;type:varchar(191);size:191;"`
-	Username      string `json:"username" form:"username" gorm:"column:username;comment:用户登录名;type:varchar(191);size:191;"`
-	Uuid          string `json:"uuid" form:"uuid" gorm:"column:uuid;comment:用户UUID;type:varchar(191);size:191;"`
+	SysUser
 }
 
 func (Student) TableName() string {
--- server/model/sys_user.go
+++ server/model/sys_user.go
@@ -8,11 +8,12 @@ import (
 type SysUser struct {
 	global.GVA_MODEL
 	UUID          uuid.UUID    `json:"uuid" gorm:"comment:用户UUID"`
-	Username      string       `json:"userName" gorm:"comment:用户登录名"`
-	Password      string       `json:"-"  gorm:"comment:用户登录密码"`
-	NickName      string       `json:"nickName" gorm:"default:系统用户;comment:用户昵称" `
-	HeaderImg     string       `json:"headerImg" gorm:"default:;comment:用户头像"`
+	Username      string       `json:"userName" form:"username" gorm:"comment:用户登录名"`
+	Password      string       `json:"-"  form:"password" gorm:"comment:用户登录密码"`
+	NickName      string       `json:"nickName" form:"nickName" gorm:"default:系统用户;comment:用户昵称" `
+	HeaderImg     string       `json:"headerImg" form:"headerImg" gorm:"default:;comment:用户头像"`
 	Authority     SysAuthority `json:"authority" gorm:"foreignKey:AuthorityId;references:AuthorityId;comment:用户角色"`
-	AuthorityId   string       `json:"authorityId" gorm:"default:888;comment:用户角色ID"`
-	HasServerInfo bool         `json:"has_server_info" gorm:"-"`
+	AuthorityId   string       `json:"authorityId" form:"authorityId" gorm:"default:888;comment:用户角色ID"`
+	HasServerInfo bool         `json:"has_server_info" gorm:"-" `
+	CoachId       uint         `json:"coachID" gorm:"coachID;comment:教练id;default:0"`
 }
